bb0e02e713f80634a330fa2b3520c0b6d66e308e
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-02 19:26:31 +0100

    Removed unnecessary methods

--- src/main/scala/app/models/image/Image.scala
+++ src/main/scala/app/models/image/Image.scala
@@ -2,76 +2,6 @@ package app.models.image
 
 import app.models.pixel.Pixel
 
-/*abstract class Image[T <: Pixel](private val __width: Int, private val __height: Int ) {
-
-	protected val _width: Int = {
-		if (__width <= 0) throw new IllegalArgumentException("Width of the image must be greater than zero")
-		__width
-	}
-	protected val _height: Int = {
-		if (__height <= 0) throw new IllegalArgumentException("Height of the image must be greater than zero")
-		__height
-	}
-
-	protected var _grid: Vector[Vector[T]]
-
-	def width: Int = _width
-	def height: Int = _height
-
-	def getRow(y: Int): Vector[T] = {
-		if (y >= _height) throwArgEx("y")
-		_grid.apply(y)
-	}
-
-	def getColumn(x: Int): Vector[T] = {
-		if (x >= _width) throwArgEx("x")
-		var col = Vector[T]()
-		for ( row <- _grid ) {
-			col = col.appended( row.apply(x) )
-		}
-		col
-	}
-
-	def getPixel(x: Int, y: Int): T = {
-		if (x >= _width) throwArgEx("x")
-		if (y >= _height) throwArgEx("y")
-
-		_grid.apply(y).apply(x)
-	}
-
-	def setRow(y: Int, newRow: Seq[T]): Unit  = {
-		if (y >= height) throwArgEx("y")
-		if (newRow.length != width) throw new IllegalArgumentException("Length of the new row must be equal to width")
-		_grid = _grid.updated(y, newRow.toVector)
-	}
-
-	def setColumn(x: Int, newColumn: Seq[T]): Unit = {
-		if (x >= width) throwArgEx("x")
-		if (newColumn.length != height) throw new IllegalArgumentException("Length of the new column must be equal to height")
-
-		for (row <- 0 until height) {
-			_grid = _grid.updated(row, _grid.apply(row).updated(x, newColumn.apply(row)))
-		}
-
-		var col = Vector[T]()
-		for (row <- _grid) {
-			col = col.appended(row.apply(x))
-		}
-		col
-	}
-
-	def setPixel(x: Int, y: Int, newPixel: T): Unit = {
-		if (x >= width) throwArgEx("x")
-		if (y >= height) throwArgEx("y")
-		_grid = _grid.updated(y, _grid.apply(y).updated(x, newPixel))
-	}
-
-	protected def throwArgEx(param: String): Unit = {
-		throw new IllegalArgumentException(s"Parameter $param is out of bounds of image size")
-	}
-
-}*/
-
 abstract class Image[T <: Pixel](private val __grid: Vector[Vector[T]]) {
 
 	protected var _grid: Vector[Vector[T]] = {
@@ -93,22 +23,24 @@ abstract class Image[T <: Pixel](private val __grid: Vector[Vector[T]]) {
 		_grid.length
 	}
 
+	def getGrid: Vector[Vector[T]] = _grid
+
 	def width: Int = _width
 	def height: Int = _height
 
-	def getRow(y: Int): Vector[T] = {
-		if (y >= _height) throwArgEx("y")
-		_grid.apply(y)
-	}
-
-	def getColumn(x: Int): Vector[T] = {
-		if (x >= _width) throwArgEx("x")
-		var col = Vector[T]()
-		_grid.foreach(row => {
-			col = col.appended(row.apply(x))
-		})
-		col
-	}
+//	def getRow(y: Int): Vector[T] = {
+//		if (y >= _height) throwArgEx("y")
+//		_grid.apply(y)
+//	}
+//
+//	def getColumn(x: Int): Vector[T] = {
+//		if (x >= _width) throwArgEx("x")
+//		var col = Vector[T]()
+//		_grid.foreach(row => {
+//			col = col.appended(row.apply(x))
+//		})
+//		col
+//	}
 
 	def getPixel(x: Int, y: Int): T = {
 		if (x >= _width) throwArgEx("x")
@@ -116,20 +48,20 @@ abstract class Image[T <: Pixel](private val __grid: Vector[Vector[T]]) {
 		_grid.apply(y).apply(x)
 	}
 
-	def setRow(y: Int, newRow: Seq[T]): Unit  = {
-		if (y >= height) throwArgEx("y")
-		if (newRow.length != width) throw new IllegalArgumentException("Length of the new row must be equal to width")
-		_grid = _grid.updated(y, newRow.toVector)
-	}
-
-	def setColumn(x: Int, newColumn: Seq[T]): Unit = {
-		if (x >= width) throwArgEx("x")
-		if (newColumn.length != height) throw new IllegalArgumentException("Length of the new column must be equal to height")
-
-		for (row <- 0 until height) {
-			_grid = _grid.updated(row, _grid.apply(row).updated(x, newColumn.apply(row)))
-		}
-	}
+//	def setRow(y: Int, newRow: Seq[T]): Unit  = {
+//		if (y >= height) throwArgEx("y")
+//		if (newRow.length != width) throw new IllegalArgumentException("Length of the new row must be equal to width")
+//		_grid = _grid.updated(y, newRow.toVector)
+//	}
+//
+//	def setColumn(x: Int, newColumn: Seq[T]): Unit = {
+//		if (x >= width) throwArgEx("x")
+//		if (newColumn.length != height) throw new IllegalArgumentException("Length of the new column must be equal to height")
+//
+//		for (row <- 0 until height) {
+//			_grid = _grid.updated(row, _grid.apply(row).updated(x, newColumn.apply(row)))
+//		}
+//	}
 
 	def setPixel(x: Int, y: Int, newPixel: T): Unit = {
 		if (x >= width) throwArgEx("x")
