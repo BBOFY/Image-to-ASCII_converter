453914c9576c28d9918d7a84e36070275b2264a4
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-02 19:25:52 +0100

    Moved

---
+++ src/main/scala/filters/Filter.scala
@@ -0,0 +1,12 @@
+package filters
+
+trait Filter[T] {
+
+	/**
+	 * Applies the specific filter on inserted image
+	 *
+	 * @param item on which filter will be applied
+	 * @return item with applied filter
+	 */
+	def apply(item: T): T
+}
