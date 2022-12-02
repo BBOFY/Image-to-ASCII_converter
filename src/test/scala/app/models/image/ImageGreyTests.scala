9a0f6248e13bd99b10294bcfe3fc663025bdf6a0
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-02 19:26:49 +0100

    Messed up

--- src/test/scala/app/models/image/ImageGreyTests.scala
+++ src/test/scala/app/models/image/ImageGreyTests.scala
@@ -33,107 +33,107 @@ class ImageGreyTests extends FunSuite {
 			image.getPixel(1, 1).value
 		}
 
-		assert(image.getColumn(2) == Vector(PixelGrey(2), PixelGrey(5)))
+//		assert(image.getColumn(2) == Vector(PixelGrey(2), PixelGrey(5)))
 	}
 
-	test ("Valid image - Changing rows") {
-		val image = new ImageGrey(testingGrid)
+//	test ("Valid image - Changing rows") {
+//		val image = new ImageGrey(testingGrid)
+//
+//		image.setRow(0, Vector( PixelGrey(100), PixelGrey(150), PixelGrey(200)))
+//
+//		assertResult(100) {
+//			image.getRow(0).apply(0).value
+//		}
+//		assertResult(150) {
+//			image.getColumn(1).apply(0).value
+//		}
+//		assertResult(200) {
+//			image.getPixel(2, 0).value
+//		}
+//		// others should not be affected
+//		assertResult(5) {
+//			image.getPixel(2, 1).value
+//		}
+//		assertResult(4) {
+//			image.getPixel(1, 1).value
+//		}
+//
+//		// row out of bounds
+//		assertThrows[IllegalArgumentException] {
+//			image.setRow(2, Vector( PixelGrey(100), PixelGrey(150), PixelGrey(200)))
+//		}
+//		// short replacement row
+//		assertThrows[IllegalArgumentException] {
+//			image.setRow(0, Vector(PixelGrey(100), PixelGrey(150)))
+//		}
+//		// wrong value inside replacement row
+//		assertThrows[IllegalArgumentException] {
+//			image.setRow(0, Vector(PixelGrey(100), PixelGrey(1500), PixelGrey(200)))
+//		}
+//
+//	}
 
-		image.setRow(0, Vector( PixelGrey(100), PixelGrey(150), PixelGrey(200)))
+//	test("Valid image - Changing columns") {
+//		val image = new ImageGrey(testingGrid)
+//
+//		image.setColumn(1, Vector( PixelGrey(10), PixelGrey(20)))
+//
+//		assertResult(10) {
+//			image.getRow(0).apply(1).value
+//		}
+//		assertResult(20) {
+//			image.getPixel(1, 1).value
+//		}
+//
+//		assertResult(0) {
+//			image.getPixel(0, 0).value
+//		}
+//		assertResult(5) {
+//			image.getPixel(2, 1).value
+//		}
+//
+//		// row out of bounds
+//		assertThrows[IllegalArgumentException] {
+//			image.setColumn(3, Vector( PixelGrey(10), PixelGrey(20)))
+//		}
+//		// long replacement row
+//		assertThrows[IllegalArgumentException] {
+//			image.setColumn(1, Vector( PixelGrey(10), PixelGrey(20), PixelGrey(30)))
+//		}
+//		// wrong value inside replacement row
+//		assertThrows[IllegalArgumentException] {
+//			image.setColumn(1, Vector( PixelGrey(1000), PixelGrey(20)))
+//		}
+//	}
 
-		assertResult(100) {
-			image.getRow(0).apply(0).value
-		}
-		assertResult(150) {
-			image.getColumn(1).apply(0).value
-		}
-		assertResult(200) {
-			image.getPixel(2, 0).value
-		}
-		// others should not be affected
-		assertResult(5) {
-			image.getPixel(2, 1).value
-		}
-		assertResult(4) {
-			image.getPixel(1, 1).value
-		}
+//	test("Valid image - Changing pixel") {
+//		val image = new ImageGrey(testingGrid)
+//
+//		for (i <- 0 until height) {
+//			image.setRow(i, testingGrid.apply(i))
+//		}
+//
+//		image.setPixel(1, 1, PixelGrey(255))
+//
+//		assertResult(255) {
+//			image.getPixel(1, 1).value
+//		}
+//	}
 
-		// row out of bounds
-		assertThrows[IllegalArgumentException] {
-			image.setRow(2, Vector( PixelGrey(100), PixelGrey(150), PixelGrey(200)))
-		}
-		// short replacement row
-		assertThrows[IllegalArgumentException] {
-			image.setRow(0, Vector(PixelGrey(100), PixelGrey(150)))
-		}
-		// wrong value inside replacement row
-		assertThrows[IllegalArgumentException] {
-			image.setRow(0, Vector(PixelGrey(100), PixelGrey(1500), PixelGrey(200)))
-		}
-
-	}
-
-	test("Valid image - Changing columns") {
-		val image = new ImageGrey(testingGrid)
-
-		image.setColumn(1, Vector( PixelGrey(10), PixelGrey(20)))
-
-		assertResult(10) {
-			image.getRow(0).apply(1).value
-		}
-		assertResult(20) {
-			image.getPixel(1, 1).value
-		}
-
-		assertResult(0) {
-			image.getPixel(0, 0).value
-		}
-		assertResult(5) {
-			image.getPixel(2, 1).value
-		}
-
-		// row out of bounds
-		assertThrows[IllegalArgumentException] {
-			image.setColumn(3, Vector( PixelGrey(10), PixelGrey(20)))
-		}
-		// long replacement row
-		assertThrows[IllegalArgumentException] {
-			image.setColumn(1, Vector( PixelGrey(10), PixelGrey(20), PixelGrey(30)))
-		}
-		// wrong value inside replacement row
-		assertThrows[IllegalArgumentException] {
-			image.setColumn(1, Vector( PixelGrey(1000), PixelGrey(20)))
-		}
-	}
-
-	test("Valid image - Changing pixel") {
-		val image = new ImageGrey(testingGrid)
-
-		for (i <- 0 until height) {
-			image.setRow(i, testingGrid.apply(i))
-		}
-
-		image.setPixel(1, 1, PixelGrey(255))
-
-		assertResult(255) {
-			image.getPixel(1, 1).value
-		}
-	}
-
-	test("Rewriting row") {
-		val image = new ImageGrey(testingGrid)
-
-		image.getRow(0).map({pixel => PixelGrey(pixel.value + 2)})
-
-		assert(
-			image.getRow(0) == Vector(PixelGrey(0), PixelGrey(1), PixelGrey(2))
-		)
-
-		assert(
-			image.getRow(0).map({pixel => PixelGrey(pixel.value + 2)})
-			  == Vector(PixelGrey(2), PixelGrey(3), PixelGrey(4))
-		)
-	}
+//	test("Rewriting row") {
+//		val image = new ImageGrey(testingGrid)
+//
+//		image.getRow(0).map({pixel => PixelGrey(pixel.value + 2)})
+//
+//		assert(
+//			image.getRow(0) == Vector(PixelGrey(0), PixelGrey(1), PixelGrey(2))
+//		)
+//
+//		assert(
+//			image.getRow(0).map({pixel => PixelGrey(pixel.value + 2)})
+//			  == Vector(PixelGrey(2), PixelGrey(3), PixelGrey(4))
+//		)
+//	}
 
 //	private def initTestImage(w: Int, h: Int): ImageGrey = {
 //
