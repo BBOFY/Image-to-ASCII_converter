d1b539ff658a91a89a1d5f4579c5242c21fbeeb3
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-02 15:11:54 +0100

    Playing with call order

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,27 +1,48 @@
-import scala.io.Source
 
-import java.io.File
+import app.handlers.ImportHandler
+import app.handlers.importHandlers.{ImportErrorHandler, ImportJpgHandler, ImportPngHandler}
+import app.processor.ImageProcessorImpl
+
+import scala.io.Source
+import java.io.{File, PrintStream}
 import javax.imageio.ImageIO
 import java.awt.Image
 import java.awt.image.BufferedImage
 
 object Main {
+	private val imageProcessor = ImageProcessorImpl
+	private val outStream = new PrintStream(System.out)
 
 	def main(args: Array[String]): Unit = {
 
+
+
+		// filter args
+
+		// call import handler
+
+		// change image to greyscale (can be as a filter at the beginning)
+
+		// call filter handler
+		// or
+		// call filters (from image processor)
+
+		// call image converter
+
+		// call export handler
+
 	}
 
-//	def convertToBufferedImage(img: Image): BufferedImage = {
-//		img match {
-//			case image: BufferedImage => return image
-//			case _ =>
-//		}
-//		// Create a buffered image with transparency
-//		val bi = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB)
-//		val graphics2D = bi.createGraphics
-//		graphics2D.drawImage(img, 0, 0, null)
-//		graphics2D.dispose()
-//		bi
-//	}
+	def importHandlers(): ImportHandler = {
 
+		val importJpgHandler = new ImportJpgHandler(imageProcessor)
+		val importPngHandler = new ImportPngHandler(imageProcessor)
+
+		val initialImportHandler: ImportHandler = importJpgHandler
+		initialImportHandler
+		  .setNext(importPngHandler)
+		  .setNext(new ImportErrorHandler(outStream))
+
+		initialImportHandler
+	}
 }
