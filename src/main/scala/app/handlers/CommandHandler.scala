03e1a951f2911ca1c84a4098b3c78ee975bd59e9
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-02 14:27:20 +0100

    Renamed

--- src/main/scala/app/handlers/CommandHandler.scala
+++
@@ -1,8 +0,0 @@
-package app.handlers
-
-trait CommandHandler extends SimpleHandler[String] {
-
-	def validCommandForms: Seq[String]
-	protected def processCommand(args: Seq[String]): Unit
-	def handle(command: String): Option[Handler[String]]
-}
