e0973a8a8325bc1b457c8df55ad9d43f99d24270
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-02 15:13:41 +0100

    Fixed type

--- src/main/scala/app/handlers/importHandlers/ImportErrorHandler.scala
+++ src/main/scala/app/handlers/importHandlers/ImportErrorHandler.scala
@@ -4,9 +4,9 @@ import app.handlers.{Handler, SimpleHandler}
 
 import java.io.OutputStream
 
-class ImportErrorHandler(out: OutputStream) extends SimpleHandler[Nothing] {
+class ImportErrorHandler(out: OutputStream) extends SimpleHandler[String] {
 
-	override def handle(args: Nothing): Option[Handler[Nothing]] = {
+	override def handle(args: String): Option[Handler[String]] = {
 		out.write("Unsupported file extension".getBytes())
 		out.flush()
 		super.handle(args)
