b1b1b6ced31838f09d4446af5530245f322e52b5
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-02 15:10:31 +0100

    Initial commit

---
+++ src/test/scala/app/importers/ImageJpgImporterTests.scala
@@ -0,0 +1,18 @@
+package app.importers
+
+import app.models.image.ImageRgb
+import org.scalatest.FunSuite
+
+class ImageJpgImporterTests extends FunSuite {
+
+	test("importer") {
+
+		val importer = new ImporterJpg("src/main/resources/test_jpg.jpg")
+		val img: ImageRgb = importer.doImport()
+
+		println(img.getPixel(1, 0).value)
+
+
+	}
+
+}
---
+++ src/test/scala/app/importers/ImagePngImporterTests.scala
@@ -0,0 +1,21 @@
+package app.importers
+
+import app.models.image.ImageRgb
+import org.scalatest.FunSuite
+
+class ImagePngImporterTests extends FunSuite {
+
+	test("importer") {
+
+		val importer = new ImporterPng("src/main/resources/test_png.png")
+		val img: ImageRgb = importer.doImport()
+
+		for (x <- 0 until img.width) {
+			for (y <- 0 until img.height) {
+				println(img.getPixel(x, y).value)
+			}
+		}
+
+	}
+
+}
