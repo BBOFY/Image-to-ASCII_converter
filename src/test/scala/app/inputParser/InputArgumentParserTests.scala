deea4a6f27a21b171054420ec1e439c016c186d9
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-05 23:48:55 +0100

    Predefined tests

--- src/test/scala/app/inputParser/InputArgumentParserTests.scala
+++ src/test/scala/app/inputParser/InputArgumentParserTests.scala
@@ -1,7 +1,25 @@
 package app.inputParser
 
+import app.models.commands.DefaultCommands
 import org.scalatest.FunSuite
 
 class InputArgumentParserTests extends FunSuite {
-	assert(List(1, 2, 3) == List(3, 2, 1))
+	val commandSet = new DefaultCommands
+
+	test("Valid 0") {}
+	test("Valid 1") {}
+	test("Valid 2") {}
+	test("Valid 3") {}
+	test("Valid 4") {}
+	test("Multiple image command") {}
+	test("Multiple table command") {}
+	test("Missing argument 0") {}
+	test("Missing argument 1") {}
+	test("Missing last argument") {}
+	test("Extra argument 0") {}
+	test("Extra argument 1") {}
+	test("No arguments") {}
+	// TODO: create generator
+	test("Random argument order") {}
+
 }
