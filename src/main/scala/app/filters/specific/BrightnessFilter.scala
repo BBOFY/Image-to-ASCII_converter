84cbace9b3b8aca175e08b565366f425fb249032
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-05 14:30:53 +0100

    Implemented logic

--- src/main/scala/app/filters/specific/BrightnessFilter.scala
+++ src/main/scala/app/filters/specific/BrightnessFilter.scala
@@ -1,39 +1,36 @@
-//package app.filters.specific
-//
-//import app.filters.ImageFilter
-//import app.models.image.ImageGrey
-//
-//
-//class BrightnessFilter(private val increment: Int) extends ImageFilter {
-//
-//	override def apply(image: ImageGrey): ImageGrey = {
-//
-////		val imgGrid = image.getGrid
-////
-////		imgGrid.map(row => {
-////			row.map( pixel => {
-////				pixel.value_
-////			} )
-////		})
-//
-//		image
-//
-//
-//	}
-//
-////	def foo(): Image[Pixel] = {
-////
-////		val testingGrid: Vector[Vector[PixelGrey]] = Vector(
-////			Vector(PixelGrey(0), PixelGrey(1), PixelGrey(2)),
-////			Vector(PixelGrey(3), PixelGrey(4), PixelGrey(5))
-////		)
-////		new ImageGrey(testingGrid)
-////
-////	}
-//
-//}
-//
-////Type mismatch.
-////Required: Vector[Vector[Pixel]]
-////   found: Vector[Vector[PixelGrey]]
-//
+package app.filters.specific
+
+import app.filters.ImageFilter
+import app.models.image.ImageGrey
+import app.models.pixel.PixelGrey
+
+
+class BrightnessFilter(private val increment: Int) extends ImageFilter {
+
+	override def apply(image: ImageGrey): ImageGrey = {
+
+		var newGrid: Vector[Vector[PixelGrey]] = Vector.empty
+
+		for (row <- image.getGrid) {
+			var newRow: Vector[Vector[PixelGrey]] = Vector.empty
+			for (pixel <- row) {
+				val brightenedPixel = brightenPixel(increment, pixel)
+				newRow = newRow.appended(brightenedPixel)
+			}
+			newGrid = newGrid.appended(newRow)
+		}
+
+		new ImageGrey(newGrid)
+
+	}
+
+	protected def brightenPixel(brightnessInc: Int, pixel: PixelGrey): PixelGrey = {
+		val newPixelValue = pixel.value_ + brightnessInc
+		newPixelValue match {
+			case x if x <= 0 => PixelGrey(0)
+			case x if x >= 0 => PixelGrey(255)
+			case _ => PixelGrey(newPixelValue)
+		}
+	}
+}
+
--- src/main/scala/app/filters/specific/InvertFilter.scala
+++ src/main/scala/app/filters/specific/InvertFilter.scala
@@ -1,5 +1,32 @@
-//package app.filters.specific
-//
-//class InvertFilter extends {
-//
-//}
+package app.filters.specific
+
+import app.filters.ImageFilter
+import app.models.image.ImageGrey
+import app.models.pixel.PixelGrey
+
+class InvertFilter extends ImageFilter {
+	/**
+	 * Applies the specific filter on inserted image
+	 *
+	 * @param image on which filter will be applied
+	 * @return item with applied filter
+	 */
+	override def apply(image: ImageGrey): ImageGrey = {
+		var newGrid: Vector[Vector[PixelGrey]] = Vector.empty
+
+		for(row <- image.getGrid) {
+			var newRow: Vector[Vector[PixelGrey]] = Vector.empty
+			for (pixel <- row) {
+				newRow = newRow.appended(invertPixel(pixel))
+			}
+			newGrid = newGrid.appended(newRow)
+		}
+
+		new ImageGrey(newGrid)
+	}
+
+	protected def invertPixel(pix: PixelGrey): PixelGrey = {
+		val newVal = 255 - pix.value_
+		PixelGrey(newVal)
+	}
+}
