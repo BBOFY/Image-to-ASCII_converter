ae11753cef10e17e53a5f0af78444f3f918f7d2e
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-07 14:40:47 +0100

    Removed

--- src/main/scala/app/handlers/converterHandlers/ConvertsionHandler.scala
+++
@@ -1,8 +0,0 @@
-package app.handlers.converterHandlers
-
-import app.handlers.{Handler, SimpleHandler}
-
-trait ConvertsionHandler extends SimpleHandler[List[String]] {
-
-	def handle(args: List[String]): Option[Handler[List[String]]]
-}
