6d21fb92085ef091261f4effebb9be70cc90019f
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-07 23:06:23 +0100

    Small refactor

--- src/main/scala/app/models/pixel/PixelRgb.scala
+++ src/main/scala/app/models/pixel/PixelRgb.scala
@@ -1,7 +1,5 @@
 package app.models.pixel
 
-import app.models.utils.RgbValue
-
 final case class PixelRgb(private val r: Int, private val g: Int, private val b: Int) extends Pixel {
 
 	@throws (classOf[ExceptionInInitializerError])
