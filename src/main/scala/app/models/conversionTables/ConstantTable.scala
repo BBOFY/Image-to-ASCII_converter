ba8e626aa312fb718530e204432084e916d8bf50
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-07 12:09:57 +0100

    Implemented conversion tables

---
+++ src/main/scala/app/models/conversionTables/ConstantTable.scala
@@ -0,0 +1,5 @@
+package app.models.conversionTables
+
+class ConstantTable(maxValue: Int) extends NonLinearTable(maxValue) {
+	override val _table = "qwerty0uiopa ,./;'\\]9[sdfghjkl1z2x3c4v5b6n7m8"
+0}
