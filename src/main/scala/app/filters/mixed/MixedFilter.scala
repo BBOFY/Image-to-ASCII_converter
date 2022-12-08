17f301ec800f12e348ce852148f3b3f8e7e89d70
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-08 12:02:45 +0100

    Implemented filter tests

--- src/main/scala/app/filters/mixed/MixedFilter.scala
+++ src/main/scala/app/filters/mixed/MixedFilter.scala
@@ -11,15 +11,10 @@ class MixedFilter (filters: Seq[ImageFilter] = Seq(IdentityImageFilter)) extends
 	 * @return item with applied filter
 	 */
 	override def apply(item: ImageGrey): ImageGrey = {
-//		filters.foldLeft(item)((accumulator, filter) => {
-//			filter.apply(accumulator)
-//		})
-
 		var image = item
 		filters.foreach(filter => {
 			image = filter.apply(image)
 		})
-
 		image
 	}
 }
