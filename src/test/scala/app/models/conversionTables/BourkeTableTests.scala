acd635e27498a37ab7e9662e1aa49a530ad1cec6
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-08 09:18:42 +0100

    Implemented test

--- src/test/scala/app/models/conversionTables/BourkeTableTests.scala
+++ src/test/scala/app/models/conversionTables/BourkeTableTests.scala
@@ -3,8 +3,42 @@ package app.models.conversionTables
 import org.scalatest.FunSuite
 
 class BourkeTableTests extends FunSuite {
-	private val table = BourkeTable
+	private val table = BourkeTable.apply()
 
-	test("")
+	validValueTest(0, '$')
+	validValueTest(11, 'B')
+	validValueTest(42, 'a')
+	validValueTest(79, 'O')
+	validValueTest(100, 'U')
+	validValueTest(148, '/')
+	validValueTest(193, '_')
+	validValueTest(218, '!')
+	validValueTest(255, ' ')
+
+
+	invalidValueTest(-2)
+	invalidValueTest(195318)
+	invalidValueTest(Int.MaxValue)
+	invalidValueTest(Int.MinValue)
+
+	test("Whole image") {
+
+	}
+
+	private def validValueTest(initial: Int, expected: Char): Unit = {
+		test(s"Valid value $initial") {
+			assertResult(expected) {
+				table.getTableValue(initial)
+			}
+		}
+	}
+
+	private def invalidValueTest(initial: Int): Unit = {
+		test(s"Invalid value $initial") {
+			assertThrows[IllegalArgumentException] {
+				table.getTableValue(initial)
+			}
+		}
+	}
 
 }
