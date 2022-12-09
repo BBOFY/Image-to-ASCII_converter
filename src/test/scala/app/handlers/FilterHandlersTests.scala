e21ed7da29458c5c364c4989cfc202e9cb62d5a4
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-09 22:02:30 +0100

    Implemented filter handlers tests

--- src/test/scala/app/handlers/FilterHandlersTests.scala
+++ src/test/scala/app/handlers/FilterHandlersTests.scala
@@ -2,6 +2,7 @@ package app.handlers
 
 import app.builders.FilterBuilder
 import app.filters.ImageFilter
+import app.filters.specific.{BrightnessFilter, FlipXFilter, FlipYFilter, InvertFilter, RotateFilter}
 import app.handlers.filterHandlers.{BrightnessFilterHandler, FilterHandler, FlipXFilterHandler, FlipYFilterHandler, InvertFilterHandler, RotateFilterHandler}
 import app.inputParser.InputArgumentsParser
 
@@ -19,13 +20,121 @@ class FilterHandlersTests extends HandlerTests {
 
 	private val builder = new DummyFilterBuilder
 
-	private val args0: Seq[String] = Seq("--table", "constant")
-	private val args1: Seq[String] = Seq("--table", "bourke", "--custom-table", "abcd")
-	private val args2: Seq[String] = Seq("--table", "bourke", "--custom-table")
-	private val args3: Seq[String] = Seq("--custom-table", "whatever", "command")
-	private val args4: Seq[String] = Seq("--table", "constant")
-	private val args5: Seq[String] = Seq("--table", "constant")
-	private val args6: Seq[String] = Seq("--table", "constant")
+	private val args0: Seq[String] = Seq("--invert", "--flip", "x")
+	private val args1: Seq[String] = Seq("--rotate", "+90", "--rotate", "-270")
+	private val args2: Seq[String] = Seq("--brightness", "+84", "--flip", "y", "--flip", "y")
+	private val args3: Seq[String] = Seq("--invert", "whatever")
+	private val args4: Seq[String] = Seq("--flip", "x", "y")
+	private val args5: Seq[String] = Seq("--rotate", "91", "--invert")
+	private val args6: Seq[String] = Seq("--invert", "--flip", "--invert")
+
+	private val rotateFilter90 = new RotateFilter
+	rotateFilter90.setValue(90)
+	private val rotateFilter_270 = new RotateFilter
+	rotateFilter_270.setValue(-270)
+	private val brightFilter84 = new BrightnessFilter
+	brightFilter84.setValue(84)
+
+	private val refFilters0: Seq[ImageFilter] = Seq(new InvertFilter, new FlipXFilter)
+	private val refFilters1: Seq[ImageFilter] = Seq(rotateFilter90, rotateFilter_270)
+	private val refFilters2: Seq[ImageFilter] = Seq(brightFilter84, new FlipYFilter, new FlipYFilter)
+	private val refFilters3: Seq[ImageFilter] = Seq(new InvertFilter)
+	private val refFilters4: Seq[ImageFilter] = Seq(new FlipXFilter)
+	private val refFilters5: Seq[ImageFilter] = Seq.empty
+	private val refFilters6: Seq[ImageFilter] = Seq(new InvertFilter)
+
+
+	test("Valid input 0") {
+		builder.counter = 0
+		builder.filters = Seq.empty
+		val parser = new InputArgumentsParser(args0)
+		val handlers = filterHandlers(parser)
+
+		assert(!parser.argsEmpty())
+		callArgs(handlers, parser)
+		assert(parser.argsEmpty())
+		assert(parser.getArgs == Seq.empty)
+		assert(builder.counter == 2)
+		assert(builder.filters == refFilters0)
+	}
+	test("Valid input 1") {
+		builder.counter = 0
+		builder.filters = Seq.empty
+		val parser = new InputArgumentsParser(args1)
+		val handlers = filterHandlers(parser)
+
+		assert(!parser.argsEmpty())
+		callArgs(handlers, parser)
+		assert(parser.argsEmpty())
+		assert(parser.getArgs == Seq.empty)
+		assert(builder.counter == 2)
+		assert(builder.filters == refFilters1)
+	}
+	test("Valid input 2") {
+		builder.counter = 0
+		builder.filters = Seq.empty
+		val parser = new InputArgumentsParser(args2)
+		val handlers = filterHandlers(parser)
+
+		assert(!parser.argsEmpty())
+		callArgs(handlers, parser)
+		assert(parser.argsEmpty())
+		assert(parser.getArgs == Seq.empty)
+		assert(builder.counter == 3)
+		assert(builder.filters == refFilters2)
+	}
+	test("Input with unknown command") {
+		builder.counter = 0
+		builder.filters = Seq.empty
+		val parser = new InputArgumentsParser(args3)
+		val handlers = filterHandlers(parser)
+
+		assert(!parser.argsEmpty())
+		callArgs(handlers, parser)
+		assert(!parser.argsEmpty())
+		assert(parser.getArgs == Seq("whatever"))
+		assert(builder.counter == 1)
+		assert(builder.filters == refFilters3)
+	}
+	test("Input with too many arguments") {
+		builder.counter = 0
+		builder.filters = Seq.empty
+		val parser = new InputArgumentsParser(args4)
+		val handlers = filterHandlers(parser)
+
+		assert(!parser.argsEmpty())
+		callArgs(handlers, parser)
+		assert(!parser.argsEmpty())
+		assert(parser.getArgs == Seq("y"))
+		assert(builder.counter == 1)
+		assert(builder.filters == refFilters4)
+	}
+	test("Input with invalid rotation") {
+		builder.counter = 0
+		builder.filters = Seq.empty
+		val parser = new InputArgumentsParser(args5)
+		val handlers = filterHandlers(parser)
+
+		assert(!parser.argsEmpty())
+		callArgs(handlers, parser)
+		assert(!parser.argsEmpty())
+		assert(parser.getArgs == Seq("--rotate", "91", "--invert"))
+		assert(builder.counter == 0)
+		assert(builder.filters == refFilters5)
+	}
+	test("Input with missing argument rotation") {
+		builder.counter = 0
+		builder.filters = Seq.empty
+		val parser = new InputArgumentsParser(args6)
+		val handlers = filterHandlers(parser)
+
+		assert(!parser.argsEmpty())
+		callArgs(handlers, parser)
+		assert(!parser.argsEmpty())
+		assert(parser.getArgs == Seq("--flip", "--invert"))
+		assert(builder.counter == 1)
+		assert(builder.filters == refFilters6)
+	}
 
 
 	private def filterHandlers(parser: InputArgumentsParser): FilterHandler = {
