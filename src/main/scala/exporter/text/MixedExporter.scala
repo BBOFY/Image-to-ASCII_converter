cd47c92866a65ee769fe03f6c37882eb4ed4d7ca
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-09 19:18:00 +0100

    Removed

--- src/main/scala/exporter/text/MixedExporter.scala
+++
@@ -1,9 +0,0 @@
-//package exporter.text
-//
-//class MixedExporter(exporters: Seq[TextExporter]) extends TextExporter {
-//	override def `export`(item: String): Unit = {
-//		exporters.foreach(exporter => {
-//			exporter.`export`(item)
-//		})
-//	}
-//}
