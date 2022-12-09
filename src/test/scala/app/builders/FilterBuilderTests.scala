16a9b14e0f39b73fd0bc10968ef5d82e7211c6ad
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-09 13:24:46 +0100

    Implemented tests

--- src/test/scala/app/builders/FilterBuilderTests.scala
+++ src/test/scala/app/builders/FilterBuilderTests.scala
@@ -1,25 +1,80 @@
 package app.builders
 
-import app.filters.{ImageFilter, VariableFilter}
+import app.filters.mixed.MixedFilter
+import app.filters.{IdentityImageFilter, ImageFilter, VariableFilter}
 import app.filters.specific.{BrightnessFilter, FlipXFilter, FlipYFilter, InvertFilter, RotateFilter}
-import app.models.image.ImageGrey
-import app.models.pixel.PixelGrey
 import org.scalatest.FunSuite
 
 class FilterBuilderTests extends FunSuite {
-	protected val builder = new FilterBuilder
 
-	protected val filterInv: ImageFilter = new InvertFilter
-	protected val filterFlipX: ImageFilter = new FlipXFilter
-	protected val filterFlipY: ImageFilter = new FlipYFilter
-	protected val filterRotate90: VariableFilter = new RotateFilter
-	filterRotate90.setValue(90)
-	protected val filterRotate180: VariableFilter = new RotateFilter
-	filterRotate180.setValue(180)
-	protected val filterBrightness: VariableFilter = new BrightnessFilter
-	filterBrightness.setValue(25)
+	protected val identity: ImageFilter = IdentityImageFilter
+	protected val flipperX: ImageFilter = new FlipXFilter
+	protected val flipperY: ImageFilter = new FlipYFilter
+	protected val inverter: ImageFilter = new InvertFilter
+	protected val brightener: VariableFilter = new BrightnessFilter
+	brightener.setValue(25)
+	protected val rotator90: VariableFilter = new RotateFilter
+	rotator90.setValue(90)
+	protected val rotator180: VariableFilter = new RotateFilter
+	rotator180.setValue(180)
 
+	protected val rotateFilter: Seq[ImageFilter] = Seq(rotator180)
+	protected val invertFilter: Seq[ImageFilter] = Seq(inverter)
+	protected val flipRotateFilters: Seq[ImageFilter] = Seq(flipperX, rotator90)
+	protected val brightnessInvertFilters: Seq[ImageFilter] = Seq(brightener, identity, inverter)
+	protected val flipRotateInvertRotateFlipFilters: Seq[ImageFilter] = Seq(flipperY, rotator180, inverter, rotator90, flipperX)
 
+	protected val emptyMF = new MixedFilter
+	protected val rotationMF = new MixedFilter(rotateFilter)
+	protected val invertMF = new MixedFilter(invertFilter)
+	protected val flipRotateMF = new MixedFilter(flipRotateFilters)
+	protected val brightnessInvertMF = new MixedFilter(brightnessInvertFilters)
+	protected val flipRotateInvertRotateFlipMF = new MixedFilter(flipRotateInvertRotateFlipFilters)
 
+	test("Default filter") {
+		val builder = new FilterBuilder
+		assert(builder.build == emptyMF)
+	}
+	test("Simple filter 0") {
+		val builder = new FilterBuilder
+		builder.registerProperty(rotator180)
+		assert(builder.build == rotationMF)
+	}
+	test("Simple filter 1") {
+		val builder = new FilterBuilder
+		builder.registerProperty(inverter)
+		assert(builder.build == invertMF)
+	}
+	test("Flip Rotate") {
+		val builder = new FilterBuilder
+		builder.registerProperty(flipperX)
+		builder.registerProperty(rotator90)
+		assert(builder.build == flipRotateMF)
+	}
+	test("Bright Identity Invert") {
+		val builder = new FilterBuilder
+		builder.registerProperty(brightener)
+		builder.registerProperty(identity)
+		builder.registerProperty(inverter)
+		assert(builder.build == brightnessInvertMF)
+	}
+	test("Complex Filter") {
+		val builder = new FilterBuilder
+		builder.registerProperty(flipperY)
+		builder.registerProperty(rotator180)
+		builder.registerProperty(inverter)
+		builder.registerProperty(rotator90)
+		builder.registerProperty(flipperX)
+		assert(builder.build == flipRotateInvertRotateFlipMF)
+	}
+
+	test("Complex Filter identity removed") {
+		val builder = new FilterBuilder
+		builder.registerProperty(flipperY)
+		builder.registerProperty(rotator180)
+		builder.registerProperty(rotator90)
+		builder.registerProperty(flipperX)
+		assert(builder.build != flipRotateInvertRotateFlipMF)
+	}
 
 }
