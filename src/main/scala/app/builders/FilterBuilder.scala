130d35b51d6e2db6bd90409192139aafbc99dd6f
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-09 13:24:56 +0100

    Fixed adding of new filters

--- src/main/scala/app/builders/FilterBuilder.scala
+++ src/main/scala/app/builders/FilterBuilder.scala
@@ -5,12 +5,16 @@ import app.filters.mixed.MixedFilter
 
 class FilterBuilder extends Builder[ImageFilter, ImageFilter] {
 
-	private var filters: Seq[ImageFilter] = Seq(IdentityImageFilter)
+	private var filters: Seq[ImageFilter] = Seq.empty
 
 	override def registerProperty(filter: ImageFilter): Unit = {
 		filters = filters.appended(filter)
 	}
 
-	override def build: ImageFilter = new MixedFilter(filters)
+	override def build: ImageFilter = {
+		if (filters.isEmpty)
+			filters = Seq(IdentityImageFilter)
+		new MixedFilter(filters)
+	}
 
 }
