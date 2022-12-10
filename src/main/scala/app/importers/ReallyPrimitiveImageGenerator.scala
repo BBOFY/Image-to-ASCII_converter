fe8bb952b4385678e259d7e82251c870dd2466a6
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-10 15:45:58 +0100

    Implemented slightly more complicated image generation

--- src/main/scala/app/importers/ReallyPrimitiveImageGenerator.scala
+++ src/main/scala/app/importers/ReallyPrimitiveImageGenerator.scala
@@ -2,9 +2,26 @@ package app.importers
 import app.models.image.ImageRgb
 import app.models.pixel.PixelRgb
 
+import scala.util.Random
+
 class ReallyPrimitiveImageGenerator extends ImportRandom {
 	override def doImport(): ImageRgb = {
-		val grid = Vector(Vector(PixelRgb(42, 6, 9)))
+
+		var grid: Vector[Vector[PixelRgb]] = Vector.empty
+
+
+		for (y <- 0 to Random.between(0, 400)) {
+			var row: Vector[Vector[PixelRgb]] = Vector.empty
+			for (x <- 0 to Random.between(0, 400)) {
+				val r = Random.between(0, 256)
+				val g = Random.between(0, 256)
+				val b = Random.between(0, 256)
+				val pixel = PixelRgb(r, g, b)
+				row = row.appended(pixel)
+			}
+			grid = grid.appended(row)
+		}
+
 		new ImageRgb(grid)
 	}
 }
