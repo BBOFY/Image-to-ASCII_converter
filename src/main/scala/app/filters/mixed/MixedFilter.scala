6b10449960aee2619fbadcf08d83972d791132dd
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-10 19:15:40 +0100

    Adding documentation

--- src/main/scala/app/filters/mixed/MixedFilter.scala
+++ src/main/scala/app/filters/mixed/MixedFilter.scala
@@ -3,10 +3,19 @@ package app.filters.mixed
 import app.filters.{IdentityImageFilter, ImageFilter}
 import app.models.image.ImageGrey
 
+/**
+ * Filter-like structure managing multiple filters.
+ * @param filters Sequence of filter, which would be applied later. Default value is sequence only with identity filter
+ */
 class MixedFilter (val filters: Seq[ImageFilter] = Seq(IdentityImageFilter)) extends ImageFilter {
 
 	protected val _filters: Seq[ImageFilter] = filters
 
+	/**
+	 * 	Applies effect of all filters on given image
+	 *  @param item Image on which filters will be applied
+	 *  @return Image with applied filters
+	 */
 	override def apply(item: ImageGrey): ImageGrey = {
 		var image = item
 		_filters.foreach(filter => {
