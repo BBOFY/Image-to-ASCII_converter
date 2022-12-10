cf5c0e91df90aba2eb5f203a06a51e60fc40ee3e
Emil Poľák <polakemi@fit.cvut.cz>
2022-12-10 21:23:37 +0100

    Writing documentation

--- src/test/scala/app/importers/ImageJpgImporterTests.scala
+++ src/test/scala/app/importers/ImageJpgImporterTests.scala
@@ -14,7 +14,7 @@ class ImageJpgImporterTests extends ImporterTests {
 	val invalidPath = "src/main/resources/test"
 	val importer: ImporterImageIo = new ImporterJpg
 
-	// (Sub)pixel values found with GIMP2. They are not exact with ImageIO however
+	// (Sub)pixel values found with GIMP2. They are not exact with ImageIO however, even when the image is the same
 	val refImg = new ImageRgb(Vector(
 		Vector(PixelRgb(253, 0, 0), PixelRgb(10, 245, 5), PixelRgb(0, 7, 241), PixelRgb(255, 253, 248)),
 		Vector(PixelRgb(254, 255, 12), PixelRgb(255, 1, 255), PixelRgb(2, 255, 255), PixelRgb(243, 255, 251)),
